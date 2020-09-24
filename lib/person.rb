9e16556a151a9b6fd9be05cc5b17438f94b42156
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-24 20:10:04 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+  def initialize(persons_name)
+    @name = persons_name
+  end
+ 
+  def persons_name=(persons_name)
+    @name = persons_name
+  end
+ 
+  def persons_name
+    @name
+  end
+end
